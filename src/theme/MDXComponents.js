b986c3c757e3830837484b5638e706aa8e6b22fe
Gabriel Guedes <gabriel.guedes2001@gmail.com>
2026-04-17 21:55:16 -0300

    reverse

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -3,10 +3,20 @@ import MDXComponents from '@theme-original/MDXComponents';
 import { Stepper } from '@site/src/components/Stepper';
 import AboutPopover from '@site/src/components/AboutPopover';
 import Figure from '@site/src/components/Figure';
+import Columns from '@site/src/components/Columns';
+import Column from '@site/src/components/Column';
+import ImageOnClick  from '@site/src/components/ImageOnClick';
+import Timeline from '@site/src/components/Timeline';
+import TimelineItem from '@site/src/components/Timeline/TimelineItem';
 
 export default {
   ...MDXComponents,
   Stepper,
   AboutPopover,
   Figure,
+  Columns,
+  Column,
+  ImageOnClick,
+  Timeline,
+  TimelineItem,
 };
